c1b8aea9027440e42f8627c641661ea71a082c13
informatika-elfan <ackmadelfanpurnama@smkinformatika-crbn.sch.id>
2025-01-16 23:32:02 -0800

    first

---
+++ src/sistemakademik_skinfa/Sistemakademik_skinfa.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistemakademik_skinfa;
+
+/**
+ *
+ * @author Software-08
+ */
+public class Sistemakademik_skinfa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
